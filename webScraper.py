1fee77a31f6dfa2522c116b3efdacf722feafe75
Yasith Harischandra <yasith.harischandra@outlook.com>
2023-11-22 15:48:06 +0530

    Now checks if db is open before scraping data

--- webScraper.py
+++ webScraper.py
@@ -4,14 +4,12 @@ import RiyaSewanaScraper
 import persistence
 
 dataSource = persistence.postgresqlPersistence()
-dataSource.openDataSource()
+if dataSource.openDataSource():
+    myrsScraper = RiyaSewanaScraper.RiyaSewanaScraper(dataSource)
+    myrsScraper.traverseSiteFrontToBack()
+    #myrsScraper.traverseSiteBackToFront()
 
+    #myrsScraper.extractVehicleData("https://riyasewana.com/buy/mitsubishi-canter-sale-mulleriyawa-7223998")
 
-myrsScraper = RiyaSewanaScraper.RiyaSewanaScraper(dataSource)
-myrsScraper.traverseSiteFrontToBack()
-#myrsScraper.traverseSiteBackToFront()
-
-#myrsScraper.extractVehicleData("https://riyasewana.com/buy/mitsubishi-canter-sale-mulleriyawa-7223998")
-
-dataSource.closeDataSource()
+    dataSource.closeDataSource()
 
